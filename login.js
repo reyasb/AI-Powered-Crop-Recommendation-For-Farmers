612cbafc3fe3d77093223918ab20593e5c66ee0f
reyasb <reyasajju@gmail.com>
2026-07-24 17:02:10 +0530

    Update API URLs for production

--- login.js
+++ login.js
@@ -7,7 +7,7 @@ async function loginUser(event) {
         alert("Please fill all fields.");
         return;
     }
-    const response = await fetch("http://localhost:5000/api/login", {
+    const response = await fetch("/api/login", {
         method: "POST",
         headers: {
             "Content-Type": "application/json"
--- register.js
+++ register.js
@@ -24,7 +24,7 @@ async function registerUser(event){
 
     }
 
-    const response = await fetch("http://localhost:5000/api/register",{
+    const response = await fetch("/api/register", {
 
         method:"POST",
 
